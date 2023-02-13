34f002cd184bfb5cd1ddad18ee157d0a522ba58a
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-13 07:48:17 +0300

    add more beer details

--- lib/features/beers/presentation/pages/beer_details_page.dart
+++ lib/features/beers/presentation/pages/beer_details_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class BeerDetailsPage extends StatelessWidget {
   final Beers beer;
+
   const BeerDetailsPage({Key? key, required this.beer}) : super(key: key);
 
   @override
@@ -12,28 +13,106 @@ class BeerDetailsPage extends StatelessWidget {
         elevation: 1,
         title: Text(beer.name),
       ),
-      body: Column(
-        children: [
-          Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                height: 200,
+                padding: const EdgeInsets.all(10.0),
+                width: double.infinity,
+                color: Colors.grey.withOpacity(0.2),
+                child: Expanded(child: Image.network(beer.imageUrl))),
+            Container(
+                padding: const EdgeInsets.all(10.0), child: Text(beer.tagline)),
+            Container(
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.brown.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  beer.description,
+                  textAlign: TextAlign.justify,
+                )),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.brown.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('First Brewed:'),
+                      Text(beer.firstBrewed),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Contributed by:'),
+                      Text(beer.contributedBy),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.brown.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Food Pairing:'),
+                      Text(beer.firstBrewed),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            const Text('Food Pairing'),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.brown.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
               height: 200,
-              padding: const EdgeInsets.all(10.0),
-              width: double.infinity,
-              color: Colors.grey.withOpacity(0.2),
-              child: Expanded(child: Image.network(beer.imageUrl))
-          ),
-          Container(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(beer.tagline)
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-               decoration: BoxDecoration(
-                 color: Colors.brown.withOpacity(0.2),
-                 borderRadius: BorderRadius.circular(5.0),
-               ),
-              padding: const EdgeInsets.all(10.0),
-              child: Text(beer.description, textAlign: TextAlign.justify,)),
-        ],
+              child: Flexible(
+                child: ListView.builder(
+                  itemCount: beer.foodPairing.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.brown.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(beer.foodPairing[index]),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
